3b81bb23e9de9a9cdf34fb76eb6a9d695160e46f
kachman-git <onyekachiokpala67@gmail.com>
2024-09-07 12:06:46 +0100

    First Commit

--- src/app/components/Display.tsx
+++ src/app/components/Display.tsx
@@ -8,22 +8,30 @@ type WeatherDataProps = {
 const Display = ({ weatherData }: WeatherDataProps) => {
   return (
     <div className="mt-7 text-white text-xl space-y-6">
-      <p>
-        <span className="text-orange-500 font-semibold">Country: </span>
-        {weatherData?.sys.country}
-      </p>
-      <p>
-        <span className="text-orange-500 font-semibold">Speed: </span>
-        {weatherData?.wind.speed}
-      </p>
-      <p>
-        <span className="text-orange-500 font-semibold">Latitude: </span>
-        {weatherData?.coord.lat}
-      </p>
-      <p>
-        <span className="text-orange-500 font-semibold">Longnitude: </span>
-        {weatherData?.coord.lon}
-      </p>
+      {weatherData?.sys?.country && (
+        <p>
+          <span className="text-orange-500 font-semibold">Country: </span>
+          {weatherData?.sys?.country}
+        </p>
+      )}
+      {weatherData?.wind?.speed && (
+        <p>
+          <span className="text-orange-500 font-semibold">Speed: </span>
+          {weatherData?.wind?.speed}
+        </p>
+      )}
+      {weatherData?.coord?.lat && (
+        <p>
+          <span className="text-orange-500 font-semibold">Latitude: </span>
+          {weatherData?.coord?.lat}
+        </p>
+      )}
+      {weatherData?.coord?.lon && (
+        <p>
+          <span className="text-orange-500 font-semibold">Longnitude: </span>
+          {weatherData?.coord?.lon}
+        </p>
+      )}
     </div>
   );
 };
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,11 +4,17 @@ import { WeatherData } from "../../../types/weather";
 import Display from "./Display";
 
 const Navbar = () => {
-  const [country, setCountry] = useState("nigeria");
-  const [city, setCity] = useState("lagos");
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (country.length === 0 || city.length === 0) {
+      return setError("Please Fill All Filled");
+    }
+  }, []);
 
   async function weatherFecth() {
     try {
@@ -20,8 +26,7 @@ const Navbar = () => {
       }
       const data: WeatherData = await res.json();
       setWeatherData(data);
-      console.log(data);
-
+      setError("");
       setIsLoading(false);
     } catch (error) {
       setError("There was an error ");
@@ -31,46 +36,42 @@ const Navbar = () => {
 
   const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if (country.length === 0 || city.length === 0) {
-      return setError("Please Fill All Filled");
-    }
-
     weatherFecth();
   };
 
   return (
     <div className="absolute z-50 container mx-auto p-9 ">
-      {error && <p>{error}</p>}
-      {isLoading && error ? (
-        <div className="flex justify-center items-center h-screen">
-          <div className="border border-b-4 border-t-4 border-dashed animate-spin w-12 h-12 rounded-full"></div>
+      {error && (
+        <div className="text-center text-xl font-semibold mb-5 text-orange-500">
+          {error}
         </div>
+      )}
+      <form className="flex flex-col justify-center items-center">
+        <input
+          type="text"
+          placeholder="country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="city"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button
+          className="bg-orange-500 w-full md:w-1/2 2xl:w-3/12 py-3 rounded-md outline-none mt-7 text-white font-medium"
+          onClick={handleSubmit}
+        >
+          Search
+        </button>
+      </form>
+      {isLoading ? (
+        <div>Loading...</div>
       ) : (
-        <>
-          <form className="flex flex-col justify-center items-center">
-            <input
-              type="text"
-              placeholder="country"
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="city"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-            <button
-              className="bg-orange-500 w-full md:w-1/2 2xl:w-3/12 py-3 rounded-md outline-none mt-7 text-white font-medium"
-              onClick={handleSubmit}
-            >
-              Search
-            </button>
-          </form>
-          <div className="flex flex-col mt-3 justify-center items-start lg:items-center">
-            <Display weatherData={weatherData} />
-          </div>
-        </>
+        <div className="flex flex-col mt-3 justify-center items-start lg:items-center">
+          <Display weatherData={weatherData} />
+        </div>
       )}
     </div>
   );
